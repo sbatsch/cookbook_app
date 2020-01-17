ed77c659b4584e0098d34736e927a6209b19fa37
sbatsch <sbatsch2@gmail.com>
2020-01-17 11:28:27 -0600

    added destroy action

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -1,6 +1,42 @@
 class Api::RecipesController < ApplicationController
-  def one_recipe_action 
-    @recipe = Recipe.first
-    render "one_recipe_view.json.jb"
-  end 
+    def index
+      @recipes = Recipe.all
+      render 'index.json.jb'
+    end
+
+    def create
+      @recipe = Recipe.new(
+                          title: params[:title],
+                          chef: params[:chef],
+                          ingredients: params[:ingredients],
+                          directions: params[:directions], 
+                          prep_time: params[:prep_time]
+                          )
+      @recipe.save
+      render 'show.json.jb'
+    end 
+
+    def show
+      @recipe = Recipe.find(params[:id])
+      render 'show.json.jb'
+    end 
+
+    def update 
+      @recipe = Recipe.find(params[:id])
+
+      @recipe.title = params[:title] || @recipe.title 
+      @recipe.chef = params[:chef] || @recipe.chef
+      @recipe.ingredients = params[:ingredients] || @recipe.ingredients
+      @recipe.directions = params[:directions] || @recipe.directions
+      @recipe.prep_time = params[:prep_time]  || @recipe.prep_time
+
+      @recipe.save
+      render 'show.json.jb'
+    end
+
+    def destroy
+      recipe = Recipe.find(params[:id])
+      recipe.destroy
+      render json: {message: "Recipe successfully destroyed"}
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
-    get '/one_recipe_url' => 'recipes#one_recipe_action'
+    get '/recipes' => 'recipes#index'
+    post '/recipes' => 'recipes#create'
+    get '/recipes/:id' => 'recipes#show'
+    patch '/recipes/:id' => 'recipes#update'
+    delete 'recipes/:id' => 'recipes#destroy'
   end
 end
